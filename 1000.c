994e561862e0e5681e5ade196664a8d9296df9c8
Bruna Surur <129985817+brunasurur1@users.noreply.github.com>
2024-10-15 13:49:57 -0300

    1000.c

---
+++ 1000.c
@@ -0,0 +1,10 @@
+///Bruna Surur Bergara, RA: 165536
+///Exercício Hello World do Exemplo de Torneio para entrega no dia 15/10
+
+#include <stdio.h>
+ 
+int main() 
+{
+    printf("Hello World!\n");
+    return 0;
+}
