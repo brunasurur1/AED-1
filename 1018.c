12d5bb0e0c05a1e0bb964cc84d18609f8f75b523
Bruna Surur <129985817+brunasurur1@users.noreply.github.com>
2024-12-07 17:43:29 -0300

    1018.c

---
+++ 1018.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int N, cem, cinquenta, vinte, dez, cinco, dois, um;
+
+    scanf("%d",&N);
+
+    cem = N/100;
+    cinquenta = (N%100)/50;
+    vinte = (N - cem*100 - cinquenta*50)/20;
+    dez = (N - cem*100 - cinquenta*50 - vinte*20)/10;
+    cinco = (N - cem*100 - cinquenta*50 - vinte*20 - dez*10)/5;
+    dois = (N - cem*100 - cinquenta*50 - vinte*20 - dez*10 - cinco*5)/2;
+    um = (N - cem*100 - cinquenta*50 - vinte*20 - dez*10 - cinco*5 -dois*2)/1;
+
+    printf("%d\n",N);
+    printf("%d nota(s) de R$ 100,00\n", cem);
+    printf("%d nota(s) de R$ 50,00\n", cinquenta);
+    printf("%d nota(s) de R$ 20,00\n", vinte);
+    printf("%d nota(s) de R$ 10,00\n", dez);
+    printf("%d nota(s) de R$ 5,00\n", cinco);
+    printf("%d nota(s) de R$ 2,00\n", dois);
+    printf("%d nota(s) de R$ 1,00\n", um);
+
+    return 0;
+}
